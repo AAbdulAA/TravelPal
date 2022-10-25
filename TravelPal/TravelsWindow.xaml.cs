a60d906095b80e32e1aeb10a65b1234decd7d37f
AAbdulAA <Abdulhaleem.Al-otaibi@edu.newton.se>
2022-10-25 13:12:19 +0200

    Initial commit
    Created WPF windows

---
+++ TravelPal/TravelsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TravelPal
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class TravelsWindow : Window
+    {
+        public TravelsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
